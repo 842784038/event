a596c82714ac31f2f595e30a459f53e28b1781ec
glm <1816166296@qq.com>
2023-03-09 17:37:01 +0800

    打包配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // server process.env.NODE_ENV的值就是‘development’
+  // build process.env.NODE_ENV的值就是‘production’
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
 })
